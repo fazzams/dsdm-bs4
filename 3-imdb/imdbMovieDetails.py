d62aa298bc62c56a82f11ee3295dd90cb120dbc8
Azzam <varokahdotnet@gmail.com>
2021-06-19 23:22:21 +0800

    IMDB Director (Uncompleted)

--- 3-imdb/imdbMovieDetails.py
+++ 3-imdb/imdbMovieDetails.py
@@ -1,22 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 
-movieName = input('Enter Movie Name: ')
-movieName = movieName.lower()
+res2 = requests.get('https://www.imdb.com/title/tt0111161/')
+html2 = res2.text
+soup2 = BeautifulSoup(html2, 'html.parser')
 
-res = requests.get('https://www.imdb.com/chart/top/')
-html = res.text
+summary = soup2.find('div', {'class': 'credit_summary_item'})
+print(summary.a.string)
+dirID = summary.a['href']
+dirUrl = f'https://www.imdb.com/{dirID}'
+print(dirUrl)
 
-soup = BeautifulSoup(html, 'html.parser')
-
-tbody = soup.find('tbody', {'class': 'lister-list'})
-trs = tbody.findAll('tr')
-
-for tr in trs:
-    td = tr.find('td', {'class': 'titleColumn'})
-    imdbMovieName = td.a.string.strip().lower()
-    if imdbMovieName == movieName:
-        movieId = td.a['href']
-        movieUrl = f'https://imdb.com/{movieId}'
-        print(movieUrl)
-        break
+# movieName = input('Enter Movie Name: ')
+# movieName = movieName.lower()
+#
+# res = requests.get('https://www.imdb.com/chart/top/')
+# html = res.text
+#
+# soup = BeautifulSoup(html, 'html.parser')
+#
+# tbody = soup.find('tbody', {'class': 'lister-list'})
+# trs = tbody.findAll('tr')
+#
+# for tr in trs:
+#     td = tr.find('td', {'class': 'titleColumn'})
+#     imdbMovieName = td.a.string.strip().lower()
+#     if imdbMovieName == movieName:
+#         movieId = td.a['href']
+#         movieUrl = f'https://imdb.com/{movieId}'
+#         print(movieUrl)
+#         break
